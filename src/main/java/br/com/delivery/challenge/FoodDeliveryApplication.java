9257e7ecec344d90f522ca2cc6e3fadaf2cd909e
Minumula Rajashekar <rajashekarminumula94@gmail.com>
2020-08-17 20:21:04 +0530

    Update FoodDeliveryApplication.java
    added comment3 to the master

--- src/main/java/br/com/delivery/challenge/FoodDeliveryApplication.java
+++ src/main/java/br/com/delivery/challenge/FoodDeliveryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodDeliveryApplication {
              //added comment1 to the master
 	     //added comment2 to the master
+	 //added comment3 to the master
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryApplication.class, args);
 	}
